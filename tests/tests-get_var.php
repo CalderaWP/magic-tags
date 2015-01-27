58ba337f585890606f6adc350eee4697c9908059
David Cramer <david@digilab.co.za>
2015-01-27 11:41:14 +0200

    unit tests for GET

--- tests/tests-get_var.php
+++ tests/tests-get_var.php
@@ -4,13 +4,62 @@
 /**
  * tests the _GET 
  */
-class Tests_Get_var extends WP_UnitTestCase {
+class Tests_The_Magic extends WP_UnitTestCase {
 
-	public function test_the_content() {
+	public function test_get_var() {
 		
 		$magic = new calderawp\filter\magictag();
+		$count = rand(0, 999);
+		// _GETS
+		$_GET = array(
+			'company'	=>	'caldera',
+			'name'		=>	'Bob Bitmap',
+			'count'		=>	$count
+		);
+		// REFERR
+		$_SERVER['HTTP_REFERER'] = 'http://localhost/apath/?country=ZA&location=office';
 
-		$this->assertSame( 'the date is ' . date('Y-m') , $magic->do_magic_tag( 'the date is {date:Y-m}' ) );
+
+		$this->assertSame( 'Company is caldera' 			, $magic->do_magic_tag( 'Company is {_GET:company}' ) );
+		$this->assertSame( 'Name is Bob Bitmap' 			, $magic->do_magic_tag( 'Name is {_GET:name}' ) );
+		$this->assertSame( 'Total count is ' . $count 		, $magic->do_magic_tag( 'Total count is {_GET:count}' ) );
+		$this->assertSame( 'location is office' 			, $magic->do_magic_tag( 'location is {_GET:location}' ) );
+		$this->assertSame( 'country is ZA' 					, $magic->do_magic_tag( 'country is {_GET:country}' ) );
+		
+		$this->assertSame( 'cant find {fake:name} sorry' 	, $magic->do_magic_tag( 'cant find {fake:name} sorry' ) );
+
+
+
+	}
+
+	public function test_post_var() {
+		
+		$magic = new calderawp\filter\magictag();
+		$count = rand(0, 999);
+		$_POST = array(
+			'company'	=>	'caldera',
+			'name'		=>	'Bob Bitmap',
+			'count'		=>	$count
+		);
+
+		$this->assertSame( 'Company is caldera' 			, $magic->do_magic_tag( 'Company is {_POST:company}' ) );
+		$this->assertSame( 'Name is Bob Bitmap' 			, $magic->do_magic_tag( 'Name is {_POST:name}' ) );
+		$this->assertSame( 'Total count is ' . $count 		, $magic->do_magic_tag( 'Total count is {_POST:count}' ) );
+
+	}
+
+	public function test_request_var() {
+		
+		$magic = new calderawp\filter\magictag();
+		$count = rand(0, 999);
+
+		$_REQUEST = array(
+			'company'	=>	'caldera',
+			'name'		=>	'Bob Bitmap',
+		);
+
+		$this->assertSame( 'Company is caldera' 			, $magic->do_magic_tag( 'Company is {_REQUEST:company}' ) );
+		$this->assertSame( 'Name is Bob Bitmap' 			, $magic->do_magic_tag( 'Name is {_REQUEST:name}' ) );
 
 	}
 
